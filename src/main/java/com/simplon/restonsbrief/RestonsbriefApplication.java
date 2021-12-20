6adcf160135659425793b9dbcc0217342a0bf099
meiyokraken <loic.demesmay@gmail.com>
2021-12-20 12:19:44 +0100

    init

---
+++ src/main/java/com/simplon/restonsbrief/RestonsbriefApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.restonsbrief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestonsbriefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestonsbriefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplon/restonsbrief/RestonsbriefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.restonsbrief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestonsbriefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
